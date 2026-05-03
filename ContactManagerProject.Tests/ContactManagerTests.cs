ceb8a4fd302ba130ea9cf661d43fd15808b15e92
kovaleva <kovaleva@gmail.com>
2026-05-03 22:41:57 +0300

    Добавлены тесты для классов

---
+++ ContactManagerProject.Tests/ContactManagerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ContactManagerProject.Tests
+{
+    [TestClass]
+    public class ContactManagerTests
+    {
+        private string _startDir;
+        private string _testDir;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _startDir = Directory.GetCurrentDirectory();
+            _testDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(_testDir);
+            Directory.SetCurrentDirectory(_testDir);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.SetCurrentDirectory(_startDir);
+            if (Directory.Exists(_testDir))
+            {
+                Directory.Delete(_testDir, true);
+            }
+        }
+
+        [TestMethod]
+        public void AddContact_AddsContact()
+        {
+            var manager = new ContactManager();
+            manager.AddContact(new Contact("Иван", "12345"));
+            Assert.AreEqual(1, manager.Contacts.Count);
+        }
+
+        [TestMethod]
+        public void SearchContacts_FindsByName()
+        {
+            var manager = new ContactManager();
+            manager.AddContact(new Contact("Иван", "12345"));
+            var result = manager.SearchContacts("Иван");
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
---
+++ ContactManagerProject.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ContactManagerProject.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContactManagerProject.Tests")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ea9bc565-28d8-43d2-ae55-0f93dc995f44")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
